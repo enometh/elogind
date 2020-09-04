8db2dbd824df145f08a63d1c89a5af72c5da050b
Madhu <enometh@net.meer>
2020-09-04 16:48:20 +0530

    bestiae.sedem.Sessio1.py: handle environment variables for my setup
    XDG_RUNTIME_DIR in /dev/shm/$USER (TODO on $USER and fix logfile)
    DBUS_STARTER_ADDRESS DBUS_STARTER_BUS_TYPE are set by DBus
    unset these.

--- bestiae.sedem.Sessio1.py
+++ bestiae.sedem.Sessio1.py
@@ -6,7 +6,14 @@ import os,re,logging
 from gi.repository import GLib
 from pydbus import SessionBus, SystemBus
 
-logging.basicConfig(filename='/dev/shm/dbuslog',
+os.putenv("XDG_RUNTIME_DIR", "/dev/shm/madhu/")
+
+if os.getenv("XDG_RUNTIME_DIR") == None:
+		logfile = None
+else:
+		logfile = os.path.join(os.getenv("XDG_RUNTIME_DIR"), "dbuslog")
+
+logging.basicConfig(filename=logfile,
 					filemode='w',
 					format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.DEBUG)
@@ -31,10 +38,28 @@ env_str = env_str + " HOME=/home/madhu"
 env_str = env_str + " PATH=/usr/bin:/bin:/usr/local/bin"
 env_str = env_str + " USER=madhu"
 env_str = env_str + " SHELL=/bin/zsh"
+env_str = env_str + " XDG_RUNTIME_DIR=/dev/shm/madhu"
 
+# ;madhu 200703 dbus sets
+#DBUS_STARTER_ADDRESS='unix:path=/run/dbus/system_bus_socket'
+#DBUS_STARTER_BUS_TYPE=system
+#
+# and clients like telepathy-glib try to contact the system bus
+# instead of the session bus and they fail.
+
+def unsetenv(var):
+		orig = os.getenv(var)
+		if orig != None:
+				logging.info("unset %s" % var)
+				os.unsetenv(var)
+
+unsetenv("DBUS_STARTER_ADDRESS")
+unsetenv("DBUS_STARTER_BUS_TYPE")
 
 start_cmd = env_str + " /home/madhu/misc/seat/seat0 -u madhu -s pameg -v 9 /7/gtk/emacs/build-xt/src/emacs -Q --fg-daemon=/tmp/seat0 |& tee -a /dev/shm/dbuslog &"
 stop_cmd = "sudo -u madhu emacsclient -s /tmp/seat0 --eval '(kill-emacs)' |& tee -a /dev/shm/dbuslog"
+
+# NOTES:
 # attach to emacs with emacsclient -s /tmp/seat0 -t
 # dbus-send --system --dest=bestiae.sedem.Sessio1 --print-reply /bestiae/sedem/Sessio1 bestiae.sedem.Sessio1.Start
 
