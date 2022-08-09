27821c96858cf573446c8ec89fe3f77aa6f2147b
Madhu <enometh@net.meer>
2022-08-09 11:24:38 +0530

    test.c: (test_wtfreleaselog): allow %{private}X %{public}X in formats without a segfault

--- test.c
+++ test.c
@@ -31,10 +31,26 @@ emit_log_msg(int level, char *src, const char *zone,
 	}
 }
 
+void test_wtfreleaselog();
+
 int
 main()
 {
 	use_journal = 1;
 	emit_log_msg(7, "source", "zone", 4, "message", NULL);
 
+	test_wtfreleaselog();
+	return 0;
+}
+
+
+#define _XSTRINGIFY(line) #line
+#define _STRINGIFY(line) _XSTRINGIFY(line)
+#define SD_JOURNAL_SEND(channel, priority, file, line, function, ...) do { \
+        sd_journal_send_with_location("CODE_FILE=" file, "CODE_LINE=" line, function, "WEBKIT_SUBSYSTEM=%s", "LOG_CHANNEL(channel).subsystem", "WEBKIT_CHANNEL=%s", "LOG_CHANNEL(channel).name", "PRIORITY=%i", priority, "MESSAGE=" __VA_ARGS__, NULL); \
+} while (0)
+#define RELEASE_LOG(channel, ...) SD_JOURNAL_SEND(channel, LOG_NOTICE, __FILE__, _STRINGIFY(__LINE__), __func__, __VA_ARGS__)
+#define WEBPROCESSPOOL_RELEASE_LOG_STATIC(channel, fmt, ...) RELEASE_LOG(channel, "WebProcessPool::" fmt, ##__VA_ARGS__)
+void test_wtfreleaselog() {
+            WEBPROCESSPOOL_RELEASE_LOG_STATIC("ServiceWorker", "establishRemoteWorkerContextConnectionToNetworkProcess reusing an existing web process (process=%p, workerType=%{private}s, PID=%d)", NULL, 1 ? "service" : "shared", 666);
 }
