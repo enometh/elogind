f982ef4c4fc9f66f625f8b48e487bfb9962a3221
Madhu <enometh@net.meer>
2024-01-16 13:24:13 +0530

    fixup! protect user runtime from deletion when RemoveIPC=no

--- src/login/user-runtime-dir.c
+++ src/login/user-runtime-dir.c
@@ -255,7 +255,7 @@ int user_runtime_dir(const char *verb, User *u) {
                         if (r < 0) {
                                 r = log_error_errno(r, "Failed to change ownership and mode of \"%s\": %m", u->runtime_path);
                         }
-                        (void) make_inaccessible_nodes(u->runtime_path, u->user_record->uid, u->user_record->gid);
+//                        (void) make_inaccessible_nodes(u->runtime_path, u->user_record->uid, u->user_record->gid);
                 }  else
                         r = do_mount(u->runtime_path, u->manager->runtime_dir_size, u->manager->runtime_dir_inodes,
                                      u->user_record->uid, u->user_record->gid);
